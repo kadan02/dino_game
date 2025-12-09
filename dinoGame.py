7f1f876127a54fcdeddd8c2f8d4387bbfdb84010
kadan02 <kada.norbert@gmail.com>
2025-12-09 12:36:02 +0100

    implement rectangles for player and fireball

--- dinoGame.py
+++ dinoGame.py
@@ -9,12 +9,18 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/PixelifySans-VariableFont_wght.ttf', 80)
 
 background = pygame.image.load('ui/background.png').convert()
-# Maybe add separate ground texture?
 text_surface = test_font.render("Dino Armageddon", False, "Orange").convert()
+
+# fireball object (to dodge)
 fireball_surface = pygame.image.load('ui/fireball.png').convert_alpha()
 fireball_surface_downscaled = pygame.transform.smoothscale(fireball_surface, (120, 70)).convert_alpha()
+fireball_rect = fireball_surface_downscaled.get_rect(midbottom = (1280,600)) # fireball position
 
-fireball_x_pos =  600
+# player object
+player_surface = pygame.image.load("ui/dino_walk.png").convert_alpha()
+player_surface_downscaled = pygame.transform.smoothscale(player_surface, (160, 90)).convert_alpha()
+player_rect = player_surface_downscaled.get_rect(midbottom = (80,600)) # dino position
+
 
 while True:
     for event in pygame.event.get():
@@ -25,12 +31,12 @@ while True:
     screen.blit(background, (0, 0))
     screen.blit(text_surface, (350, 300))
 
-    if fireball_x_pos < 0:
-        fireball_x_pos = 1380
+    fireball_rect.x -=3
+    if fireball_rect.right <= 0:
+        fireball_rect.left = 1280
 
-    fireball_x_pos -= 1
-
-    screen.blit(fireball_surface_downscaled, (fireball_x_pos, 250))
+    screen.blit(fireball_surface_downscaled,fireball_rect)
+    screen.blit(player_surface_downscaled,player_rect)
 
     pygame.display.update()
     clock.tick(180)
